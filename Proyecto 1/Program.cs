2dd3de6ffebfa215a356f98c27b102808439b836
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-13 08:16:10 -0700

    Agregar archivos de proyecto.

---
+++ Proyecto 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            persona persona1 = new persona(25, "José");
+
+            Console.WriteLine(persona1.nombre + "Tiene " + persona1.edad + " años");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Proyecto 1/persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_1
+{
+    class persona
+    {
+        public int edad;
+        public string nombre;
+
+       public persona(int edad, string nombre)
+        {
+            this.edad = edad;
+            this.nombre = nombre;
+        }
+
+        public persona()
+        {
+            this.edad = 0;
+            this.nombre = "";
+        }
+
+        void cumplirAno()
+        {
+            this.edad++;
+        }
+    }
+}
